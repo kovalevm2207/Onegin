6a373462ad911fdcf587d90387a90fca44f63300
kovalevm2207 <kovalevm220741@gmail.com>
2025-09-19 01:23:30 +0300

    Добавленны assert и введены более понятные названия для переменных циклов

--- Onegin_main.cpp
+++ Onegin_main.cpp
@@ -33,13 +33,13 @@ void get_text(struct line*  text, char* fixed_buffer, char* buffer);
 
 void fix_buffer (char* fixed_buffer, const char* buffer);
 
-size_t lcomp(struct line* text, size_t line1, size_t line2);
+int lcomp(struct line* text, size_t line1, size_t line2);
 
-size_t rcomp(struct line* text, size_t line1, size_t line2);
+int rcomp(struct line* text, size_t line1, size_t line2);
 
 void change(struct line* text, size_t line1, size_t line2);
 
-void bubblesort(struct line* text, size_t linenum, size_t(* comp)(struct line* text, size_t line1, size_t line2));
+void bubblesort(struct line* text, size_t linenum, int(* comp)(struct line* text, size_t line1, size_t line2));
 
 void print_text(FILE* file, struct line* text, size_t linenum);
 
@@ -73,6 +73,7 @@ int main()
     assert(linenum > 0);
 
     struct line* text = (struct line*) calloc(linenum, sizeof(line));
+    assert(text != 0);
     get_text(text, fixed_buffer, buffer);
 
     bubblesort(text, linenum, lcomp);
@@ -143,9 +144,13 @@ int main()
 
 long long find_file_size(const char* file_name)
 {
+    assert (file_name != 0);
+
     struct stat file_info;
     stat(file_name, &file_info);
+
     assert(file_info.st_size != 0);
+
     return file_info.st_size;
 }
 
@@ -160,33 +165,43 @@ size_t count_lines(char* buffer)
             linenum++;
         }
     }
+
+    assert(linenum != 0);
+
     return linenum;
 }
 
 
 void fix_buffer (char* fixed_buffer, const char* buffer)
 {
-    size_t j = 0;
-    for (size_t i = 0; buffer[i] != '\0'; i++) {
-        char c = buffer[i];
+    assert (fixed_buffer != 0);
+    assert (buffer != 0);
+
+    size_t fix = 0;
+    for (size_t base = 0; buffer[base] != '\0'; base++) {
+        char c = buffer[base];
         if (isalpha(c) != 0 || c == '\n') {
-            fixed_buffer[j] = c;
-            j++;
+            fixed_buffer[fix] = c;
+            fix++;
         }
     }
-    fixed_buffer[j] = '\0';
+    fixed_buffer[fix] = '\0';
 }
 
 
 void get_text(struct line* text, char* fixed_buffer, char* buffer)
 {
+    assert (text != 0);
+    assert (fixed_buffer != 0);
+    assert (buffer != 0);
+
     size_t count = 0, ptr_shift = 0;
 
-    for (size_t i = 0; buffer[i] != '\0'; i++) {
-        if (buffer[i] == '\n') {
+    for (size_t letter = 0; buffer[letter] != '\0'; letter++) {
+        if (buffer[letter] == '\n') {
             text[count].ptr = buffer + ptr_shift;
-            text[count].lengh = (i + 1) - ptr_shift;
-            ptr_shift = i + 1;
+            text[count].lengh = (letter + 1) - ptr_shift;
+            ptr_shift = letter + 1;
             count++;
         }
     }
@@ -205,36 +220,45 @@ void get_text(struct line* text, char* fixed_buffer, char* buffer)
 }
 
 
-void bubblesort(struct line* text, size_t linenum, size_t(* comp)(struct line* text, size_t line1, size_t line2))
+void bubblesort(struct line* text, size_t linenum, int(* comp)(struct line* text, size_t line1, size_t line2))
 {
-    for (size_t i = linenum-1; i > 1; i--) {
-        assert(i<linenum);
-        for(size_t j = 1; j <= i; j++) {
-            if (comp(text, j-1, j) == 2) {
-                change(text, j-1, j);
+    assert (text != 0);
+    assert (comp != 0);
+    assert (text != 0);
+    assert (linenum > 0);
+
+    for (size_t dlina_prohoda = linenum-1; dlina_prohoda > 1; dlina_prohoda--) {
+        assert (dlina_prohoda<linenum);
+        assert (dlina_prohoda > 1);
+        for(size_t pos = 1; pos <= dlina_prohoda; pos++) {
+            assert (pos <= dlina_prohoda);
+            if (comp(text, pos-1, pos) < 0) {
+                change(text, pos-1, pos);
             }
         }
     }
 }
 
 
-size_t lcomp(struct line* text, size_t line1, size_t line2)
+int lcomp(struct line* text, size_t line1, size_t line2)
 {
-    int i = 0;
+    assert(text != 0);
+
+    int pos = 0;
     char *str1 = text[line1].ptrfix, *str2 = text[line2].ptrfix;
-    while (str1[i] != '\n' && str2[i] != '\n') {
-        char c1 = tolower(str1[i]);
-        char c2 = tolower(str2[i]);
+    while (str1[pos] != '\n' && str2[pos] != '\n') {
+        char c1 = tolower(str1[pos]);
+        char c2 = tolower(str2[pos]);
 
         if (c1 < c2) return 1;
-        if (c1 > c2) return 2;
+        if (c1 > c2) return -1;
 
-        i++;
+        pos++;
     }
 
     // Если одна строка короче другой (раньше заканчивается)
-    if (str1[i] == '\n' && str2[i] != '\n') return 1;
-    if (str2[i] == '\n' && str1[i] != '\n') return 2;
+    if (str1[pos] == '\n' && str2[pos] != '\n') return 1;
+    if (str2[pos] == '\n' && str1[pos] != '\n') return -1;
 
     // Если строки идентичны
     return 0;
@@ -242,24 +266,25 @@ size_t lcomp(struct line* text, size_t line1, size_t line2)
 }
 
 
-size_t rcomp(struct line* text, size_t line1, size_t line2)
+int rcomp(struct line* text, size_t line1, size_t line2)
 {
-    char    *str1 = text[line1].ptrfix,         *str2 = text[line2].ptrfix;
+    assert(text != 0);
 
-    size_t lengh1 = text[line1].lenghfix,      lengh2 = text[line2].lenghfix;
+    char *str1 = text[line1].ptrfix, *str2 = text[line2].ptrfix;
+    size_t lengh1 = text[line1].lenghfix, lengh2 = text[line2].lenghfix;
 
-    for (size_t i = 0; i < (lengh2 - 1) && i < (lengh2 - 1); i++) {
+    for (size_t pos = 0; pos < (lengh2 - 1) && pos < (lengh2 - 1); pos++) {
         // Берем символы с конца строк
-        int c1 = tolower(str1[lengh1 - 2 - i]);
-        int c2 = tolower(str2[lengh2 - 2 - i]);
+        int c1 = tolower(str1[lengh1 - 2 - pos]);
+        int c2 = tolower(str2[lengh2 - 2 - pos]);
 
         if (c1 < c2) return 1;
-        if (c1 > c2) return 2;
+        if (c1 > c2) return -1;
     }
 
     // Если одна строка короче другой
     if (lengh1 < lengh2) return 1;
-    if (lengh2 < lengh1) return 2;
+    if (lengh2 < lengh1) return -1;
 
     // Если строки идентичны
     return 0;
@@ -268,6 +293,8 @@ size_t rcomp(struct line* text, size_t line1, size_t line2)
 
 void change(struct line* text, size_t line1, size_t line2)
 {
+    assert (text != 0);
+
     char* s_ptr = text[line1].ptr, *s_ptrfix = text[line1].ptrfix;
     size_t s_lengh = text[line1].lengh, s_lenghfix = text[line1].lenghfix;
 
@@ -281,6 +308,10 @@ void change(struct line* text, size_t line1, size_t line2)
 
 void print_text(FILE* file, struct line* text, size_t linenum)
 {
+    assert (file != 0);
+    assert (text != 0);
+    assert (linenum != 0);
+
     for (size_t line = 0; line < linenum; line++) {
         for (size_t let = 0; let < text[line].lengh; let++)
             fprintf(file, "%c", *(text[line].ptr + let));
